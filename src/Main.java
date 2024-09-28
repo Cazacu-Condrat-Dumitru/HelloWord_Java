5b734bd9d7035aeba5a2345f872ca848dc67d00e
Cazacu-Condrat-Dumitru <dcondrat13@gmail.com>
2024-09-28 12:18:37 +0300

    finished basic tasks

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("Hello world!11");
+        double a = 10;
+        double b = 20;
+        char c = 'A';
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a * b);
+        System.out.println(a % b);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(b +++a);
+        System.out.println(a =='A');
+        System.out.println(b ==a);
     }
 
 
