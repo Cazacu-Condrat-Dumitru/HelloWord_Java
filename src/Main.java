f17cfb6d5e07ba9342fde1a90f00eebc533c16d3
Cazacu-Condrat-Dumitru <dcondrat13@gmail.com>
2024-10-03 19:30:08 +0300

    finished second task

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 // De la tastieră se introduce simbolul direcției inițiale
 // a robotului și instrucțiunea ce trebuie îndeplinită.
 // Să se afișeze direcția de deplasare a robotului după îndeplinirea instrucțiunii
-        Scanner sc = new Scanner(System.in);
+       /* Scanner sc = new Scanner(System.in);
         System.out.println("Write the initial direction of the robot");
         String direction = String.valueOf(sc.next());
 
@@ -46,8 +46,42 @@ public class Main {
                 break;
             default:
                 System.out.println("Error");
+        }*/
+
+
+
+        /*Scrieți un program care să permită alegerea unei opțiuni dintr-un
+         anumit meniu afișat pe ecran: se afișează meniul:
+            1.  suma
+            2.  produs*/
+
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        double number1, number2, result;
+
+        System.out.println("\nSelect one of the available options (1 or 2): ");
+        System.out.println("1. Sum");
+        System.out.println("2. Multiplication");
+        option = scanner.nextInt();
+
+        System.out.print("Write the first number: ");
+        number1 = scanner.nextDouble();
+        System.out.print("Write the second number: ");
+        number2 = scanner.nextDouble();
+
+        switch (option) {
+            case 1:
+                result = number1 + number2;
+                System.out.println("The result of the sum is: " + result);
+                break;
+            case 2:
+                result = number1 * number2;
+                System.out.println("The result of the multiplication is: " + result);
+                break;
+            default:
+                System.out.println("Error. Please choose option 1 or 2");
+                break;
         }
+
     }
-
-
 }
