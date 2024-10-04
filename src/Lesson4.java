f9d5f5d73de8083ebf9d3bb9863a475091462238
Cazacu-Condrat-Dumitru <dcondrat13@gmail.com>
2024-10-04 17:55:55 +0300

    updated the project structure

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -1,9 +1,15 @@
 public class Lesson4 {
     public static void main(String[] args) {
-    int i = 1;
-    while (i < 10) {
-        System.out.println(i);
-        i++;
-    }
+        int i = 1;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("---------------------------------------------");
+        int j = 11;
+        do {
+            System.out.println(j);
+            j++;
+        } while (j <= 10);
     }
 }
